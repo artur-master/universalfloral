d00ec01c8dae4cc3d6efed6b37a7b098c989eea5
artur-master <arturshayakhmetov4@gmail.com>
2020-10-07 07:32:30 -0400

    update app.py

--- src/app.py
+++ src/app.py
@@ -20,23 +20,23 @@ def create_app(env_name):
 
   @app.route('/about')
   def about():
-    return render_template('about.html')
+    return render_template('about.html', active_page = "about")
 
   @app.route('/how-we-work')
   def howwework():
-    return render_template('/how-we-work.html')
+    return render_template('/how-we-work.html', active_page = "how-we-work")
   
   @app.route('/what-we-do')
   def whatwedo():
-    return render_template('/what-we-do.html')
+    return render_template('/what-we-do.html', active_page = "what-we-do")
   
   @app.route('/clients')
   def clients():
-    return render_template('/clients.html')
+    return render_template('/clients.html', active_page = "clients")
   
   @app.route('/contact')
   def contact():
-    return render_template('/contact.html')
+    return render_template('/contact.html', active_page = "contact")
 
   return app
 
